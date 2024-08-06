f769f546ca1b2be628ff2f1b0edd37ca2b1c99a1
MdFarhan <107925922+sret-farhan2021@users.noreply.github.com>
2024-08-06 19:52:39 +0530

    Updated some features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import ProjectDetail from './components/ProjectDetail';
+import './App.css';
 
 const projects = [
   {
@@ -29,16 +29,12 @@ const App = () => {
     <Router>
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/projects" exact>
-            <Projects projects={projects} />
-          </Route>
-          <Route path="/project/:id">
-            <ProjectDetail projects={projects} />
-          </Route>
-          <Route path="/contact" component={Contact} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects projects={projects} />} />
+          <Route path="/project/:id" element={<ProjectDetail projects={projects} />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
